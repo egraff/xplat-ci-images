233eda07f4b98fc258d70d777ede54f4bb7ab4f7
Erlend Graff <erlend.graff@gmail.com>
2020-11-09 08:45:37 +0100

    Try to build powerpc64 images

---
+++ buildroot-tree/board/powerpc64_pseries/post-build.sh
@@ -0,0 +1,18 @@
+#
+# Post-build script for buildroot
+#
+
+TARGET_ROOT_FS=$1
+printf "IN post-build script! TARGET_ROOT_FS=${TARGET_ROOT_FS}\n"
+
+chmod 644 ${TARGET_ROOT_FS}/etc/ssh/moduli
+chmod 644 ${TARGET_ROOT_FS}/etc/ssh/ssh_config
+chmod 644 ${TARGET_ROOT_FS}/etc/ssh/sshd_config
+chmod 644 ${TARGET_ROOT_FS}/etc/ssh/ssh_host_dsa_key.pub
+chmod 644 ${TARGET_ROOT_FS}/etc/ssh/ssh_host_ecdsa_key.pub
+chmod 644 ${TARGET_ROOT_FS}/etc/ssh/ssh_host_ed25519_key.pub
+chmod 644 ${TARGET_ROOT_FS}/etc/ssh/ssh_host_rsa_key.pub
+chmod 600 ${TARGET_ROOT_FS}/etc/ssh/ssh_host_dsa_key
+chmod 600 ${TARGET_ROOT_FS}/etc/ssh/ssh_host_ecdsa_key
+chmod 600 ${TARGET_ROOT_FS}/etc/ssh/ssh_host_ed25519_key
+chmod 600 ${TARGET_ROOT_FS}/etc/ssh/ssh_host_rsa_key
---
+++ buildroot-tree/board/powerpc64_pseries/post-image.sh
@@ -0,0 +1,6 @@
+#
+# Post-image script for buildroot
+#
+
+IMAGES_DIR=$1
+printf "IN post-image script! IMAGES_DIR=${IMAGES_DIR}\n"
