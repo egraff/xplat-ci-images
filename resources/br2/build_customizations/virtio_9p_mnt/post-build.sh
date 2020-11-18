91c2c441c5d7c3858c370ed45159be93505457e8
Erlend Graff <erlend.graff@gmail.com>
2020-11-18 08:48:25 +0100

    Fix typo in virtio_9p_mnt/post-build.sh

--- resources/br2/build_customizations/virtio_9p_mnt/post-build.sh
+++ resources/br2/build_customizations/virtio_9p_mnt/post-build.sh
@@ -4,7 +4,7 @@
 
 TARGET_ROOT_FS=$1
 
-if ! grep -q "hostmnt9p" "$[TARGET_ROOT_FS}/etc/fstab"; then
+if ! grep -q "hostmnt9p" "${TARGET_ROOT_FS}/etc/fstab"; then
   #     <file system>   <mount pt>      <type>  <options>       <dump>  <pass>
-  echo "hostmnt9p       /mnt            9p      trans=virtio,version=9p2000.L,rw,_netdev   0       0" >> "$[TARGET_ROOT_FS}/etc/fstab"
+  echo "hostmnt9p       /mnt            9p      trans=virtio,version=9p2000.L,rw,_netdev   0       0" >> "${TARGET_ROOT_FS}/etc/fstab"
 fi
